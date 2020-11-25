47da652238393afebf8d240c9bc130dda7630e02
tenkue-dev <rwqxh330@gmail.com>
2020-11-25 16:31:19 +0900

    postsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/update'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
