bea8349a910555eaf043a838a4115adfd87ad186
tenkue-dev <rwqxh330@gmail.com>
2020-11-26 12:59:11 +0900

    routes.rb の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-  get 'comments/create'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
-  get 'likes/create'
-  get 'likes/destroy'
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'posts/update'
+  # get 'comments/index'
+  # get 'comments/create'
+  # get 'comments/edit'
+  # get 'comments/update'
+  # get 'comments/destroy'
+  # get 'likes/create'
+  # get 'likes/destroy'
+  # get 'posts/index'
+  # get 'posts/new'
+  # get 'posts/show'
+  # get 'posts/edit'
+  # get 'posts/update'
+  resources :posts do
+    resources :likes, only:[:create, :destroy]
+    resources :comments, except:[:new, :show]
+  end 
+  root "posts#index"
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
