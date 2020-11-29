81ae9ff402ab8ecb4713106d6f8d599ad799a6e4
zakitono <coppercraft1313@gmail.com>
2020-11-29 17:32:59 +0900

    [Userモデル]name, email, passwordカラムにバリデーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :like
   has_many :post
   has_many :comment
+
+  email_format = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email, format: { with: email_format }
+  validates :password, length: { in: 8..32 }
 end
