86846b741f9e1d8403c4dc4fa4a06d16372b3a82
tenkue-dev <74527979+tenkue-dev@users.noreply.github.com>
2020-12-09 21:32:56 +0900

    Revert "Revert "掲示板投稿機能""

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,16 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
   end
 
   def show
@@ -18,5 +25,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
   end
 end
