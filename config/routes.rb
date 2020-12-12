a8a724012d81af4c9fcfa6a03575e5f921f5cb8a
tenkue-dev <rwqxh330@gmail.com>
2020-12-12 16:07:19 +0900

    不要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  # get 'comments/index'
-  # get 'comments/create'
-  # get 'comments/edit'
-  # get 'comments/update'
-  # get 'comments/destroy'
-  # get 'likes/create'
-  # get 'likes/destroy'
-  # get 'posts/index'
-  # get 'posts/new'
-  # get 'posts/show'
-  # get 'posts/edit'
-  # get 'posts/update'
   root "posts#index"
   resources :posts do
     resources :likes, only:[:create, :destroy]
