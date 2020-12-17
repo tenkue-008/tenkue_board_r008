b09ac7a33a417a58ed02563df020bf92d271ad15
tenkue-dev <rwqxh330@gmail.com>
2020-12-17 21:06:05 +0900

    未ログイン状態でも投稿を閲覧できる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only:[:new]
 
   def index
+    @post = Post.includes(:user).order(created_at: :DESC)
   end
 
   def new
